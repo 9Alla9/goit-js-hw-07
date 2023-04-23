aef70e5e71f946106622a2bb2aefff058831a6c8
9Alla9 <konfetcka@gmail.com>
2023-04-23 18:10:08 +0300

    дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const galleryContainer = document.querySelector(".gallery");
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryContainer.addEventListener("click", onImgClick);
 
 function createGalleryItemsMarkup(items) {
   return items
@@ -19,5 +21,36 @@ function createGalleryItemsMarkup(items) {
 </div>`;
     })
     .join("");
+}
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  const isItemImage = e.target.classList.contains("gallery__image");
+  if (!isItemImage) return;
+
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    const ESC_KEY_CODE = "Escape";
+    const isEscKey = e.code === ESC_KEY_CODE;
+    if (!isEscKey) return;
+    instance.close();
+  }
 }
 console.log(galleryItems);
